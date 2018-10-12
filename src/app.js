444720ee48fdcef86938995027bc305c7cc998c0
CJackYang <cjackyang@qq.com>
2018-10-12 10:54:30 +0800

    add appservice

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const AppService = (require('./services'))
+const app = express()
+
+const appService = new AppService()
+
+app.set('json spaces', 0)
+app.use(logger('dev', { skip: (req, res) => res.nolog === true || app.nolog === true }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+/* eslint-disable */
+app.use(function(err, req, res, next) {
+  if (err) {
+    console.log('::', err)
+  }
+
+  res.status(err.status || 500).json({
+    code: err.code,
+    message: err.message,
+    where: err.where
+  })
+})
+
+app.listen(8080, error => {
+  if (error) console.log('server start error. ', error)
+  // console.log('server started on port 8888')
+})
---
+++ src/services/index.js
@@ -0,0 +1,115 @@
+const request = require('superagent')
+const EventEmitter = require('events')
+
+class State {
+  
+  constructor (ctx, ...args) {
+    this.ctx = ctx
+    this.ctx.state = this
+    this.enter(...args)
+  }
+
+  setState (State, ...args) {
+    this.exit()
+    new State(this.ctx, ...args)
+  }
+
+  enter (...args) {
+
+  }
+
+  exit (...args) {
+
+  }
+}
+
+class Pending extends State {
+  
+  enter() {
+    console.log('进入等待状态...')
+    process.stdout.write('请输入KEY：')
+    process.stdin.on('data', chunk => {
+      let key = chunk.toString().trim()
+      this.setState(Checking, key)
+    })
+  }
+
+  exit() {
+    process.stdin.removeAllListeners()
+  }
+}
+
+class Failed extends State {
+  
+  enter (error) {
+    console.log('进入失败状态:')
+    console.log(error.message)
+    this.error = error
+    console.log('请重新输入KEY: ')
+    process.stdin.on('data', chunk => {
+      let key = chunk.toString().trim()
+      this.setState(Checking, key)
+    })
+  }
+
+  exit() {
+    process.stdin.removeAllListeners()
+  }
+}
+
+class Checking extends State {
+
+  enter (key) {
+    this.req = request
+      .get('localhost:8888/token')
+      .query({ key })
+      .then(res => {
+        if (res.status !== 200) {
+          return this.setState(Failed, res.error)
+        }
+        let token = res.body.token
+        this.setState(Started, token)
+      }, error => this.setState(Failed, error))
+  }
+
+  exit () {
+
+  }
+}
+
+class Started extends State {
+  
+  enter (token) {
+    process.stdout
+    console.log('****系统就绪****')
+    this.ctx.token = token
+  }
+
+  exit () {
+
+  }
+}
+
+
+class AppService extends EventEmitter {
+
+  constructor(config) {
+    super()
+    this.conf = config
+
+    this.state = new Pending(this)
+  }
+
+  registByCsr (body, callback) {
+    request
+      .post()
+      .send(body)
+      .then(res => {
+        if (res.status !== 200) return callback(res.error)
+        return callback(res.body)
+      }, callback)
+  }
+
+}
+
+module.exports = AppService
